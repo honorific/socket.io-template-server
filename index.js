1cfa03c13c6e44daef3ffbdfbdc09c74850b7a58
honorific <honorific.info@gmail.com>
2023-09-02 20:14:24 +0330

    initial config with cors enabled

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import http from 'http'
+import {Server} from 'socket.io'
+import cors from 'cors'
+
+const app = express()
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+  },
+})
